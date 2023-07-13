100bdaaa4f134eb3397939a9b21fb074940007e7
valentina vargas gutierrez <valentinagutierrez882@gmail.com>
2023-07-12 20:05:19 -0500

    DOM botones de filtros pruebas con console log

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+// este archivo contenga toda la funcionalidad 
+//que corresponda a obtener, procesar y manipular datos (tus funciones). Por ejemplo:
 // estas funciones son de ejemplo
 
 export const example = () => {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//tu código que tenga que ver con mostrar los datos en la pantalla.
+// Con esto nos referimos básicamente a la interacción con el DOM. 
+//console.log(example, data);
+
+const fCap = document.getElementById('cap');
+fCap.addEventListener("click", function(){
+    console.log("si");
+})
+
+const fLoct = document.getElementById('loct');
+fLoct.addEventListener("click", function(){
+    console.log("yass");
+})
+
+const fall = document.getElementById('allP');
+fall.addEventListener("click", function(){
+    console.log("obvi");
+})
