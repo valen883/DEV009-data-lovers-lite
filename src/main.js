4da19c54134d1290fadcf12f2abb9c3ada5b9a05
valentinagutierrez <valentinagutierrez882@gmail.com>
2023-07-21 13:44:47 -0500

    modificacion del for que pinta las cards en html, codigo repetitivo

--- src/main.js
+++ src/main.js
@@ -1,49 +1,83 @@
 import { especieF, estadF } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/rickandmorty/rickandmorty.js';
-//seleccionar la calse cardWrapper, guardar carWrapper en una constante dataTarjetas
+
 const dataTarjetas = document.querySelector(".cardWrapper");
-//const princ = familiaSanchez(data.results);
 const std = estadF(data.results);
 const spc = especieF(data.results);
 dataTarjetas.innerHTML = "";
 let todos = "";
+let especie1 = "";
 
-for ( let personaje of data.results) {
-// a dataTarjetas hacerle un dataTarjetas.innerHTML += string con index.html en card
-    todos +=` <div class="card">
-    <div class="imageContent">
-      <span class="overlay"></span>
-      <div class="cardImage">
-        <img src="${personaje.image}" alt="" class="cardImg">
-      </div>
+
+function pintarCards(personajes){
+  let cards="";
+  for ( let personaje of personajes) {
+  cards+= `<div class="card">
+  <div class="imageContent">
+    <span class="overlay"></span>
+    <div class="cardImage">
+    <img src="${personaje.image}" alt="" class="cardImg">
+    </div>
     </div>
     <div class="cardContent">
-      <p class="name">${personaje.name}</p>
-      <a class="species">${personaje.species}</a>
-      <a class="status">${personaje.status}</a> 
-      <a class="status">${personaje.origin.name}</a> 
+    <p class="name">${personaje.name}</p>
+    <a class="species">${personaje.species}</a>
+    <a class="status">${personaje.status}</a> 
+    <a class="status">${personaje.origin.name}</a> 
     </div>
-  </div>`
-  //console.log(personaje);
-} 
-dataTarjetas.innerHTML = todos;
-     
-//tu código que tenga que ver con mostrar los datos en la pantalla.
-//Con esto nos referimos básicamente a la interacción con el DOM.
+    </div>`
+  } 
+  return cards
+    } 
+  
+  todos = pintarCards(data.results);
+  const divTodos = document.createElement("div");
+  divTodos.innerHTML = todos;
+  document.getElementById("principal").appendChild(divTodos);
 
-const fSpecie = document.getElementById('esp');
-fSpecie.addEventListener("click", function(){
+  const fSpecie = document.getElementById('esp');
+  fSpecie.addEventListener("click", function(){
     document.getElementById("especie").style.display = "flex"
     document.getElementById("principal").style.display ="none"
     document.getElementById("estado").style.display = "none"
     document.getElementById("ordenar").style.display = "none"
-    console.log(spc);
+    document.getElementById("especie").innerHTML =""; //limpiar lo que haya antes
+    //crear boton
+    const botonSoloHumanos = document.createElement("button")
+    botonSoloHumanos.innerText = "Filtrar solo humanos";
+    document.getElementById("especie").appendChild(botonSoloHumanos);
+    //crear tarjetas
+    const crearTarjetas = document.createElement("div");
+    document.getElementById("especie").appendChild(crearTarjetas);
+    //filtrar por boton
+    botonSoloHumanos.addEventListener("click", () =>{
+      console.log("click");
+      //crearTarjetas.innerHTML = especie1; //.todos -> nueva variable
+      console.log(spc); //crearTarjetas.innerHTML = "variabe"
+      for ( let filter1 of spc) {
+        //pintar el html con las cards con los filtros de la funciòn en data
+            especie1 +=` <div class="card">
+            <div class="imageContent">
+              <span class="overlay"></span>
+              <div class="cardImage">
+                <img src="${filter1.image}" alt="" class="cardImg">
+              </div>
+            </div>
+            <div class="cardContent">
+            <a class="species">${filter1.species}</a>
+            </div>
+            </div>`
+          } crearTarjetas.innerHTML = especie1;
+          document.getElementById("especie").appendChild(crearTarjetas);
+    })
   })
-   
-
-const fstat = document.getElementById('stat');
-fstat.addEventListener("click", function(){
+  
+  //const crearTarjetas.innerHTML = "";
+  const crearTarjetas = document.createElement("div");
+  
+  const fstat = document.getElementById('stat');
+  fstat.addEventListener("click", function(){
   document.getElementById("estado").style.display = "flex"
   document.getElementById("principal").style.display ="none"
   document.getElementById("ordenar").style.display = "none"
