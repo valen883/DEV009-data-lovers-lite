25ef99a2fb269c74b421bf76ff621f9cc280e496
valentinagutierrez <valentinagutierrez882@gmail.com>
2023-07-24 01:04:06 -0500

    construccion de nuevos filtros y ordenar, optimizacion del codigo

--- src/data.js
+++ src/data.js
@@ -9,3 +9,29 @@ export const especieF = (data) => {
   return data.filter((item) => espec.includes(item.species));
 };
 
+//Puedes usar el método sort() junto a localeCompare() 
+export const orderData = (data,sorting) => {
+  const field = sorting.field
+  if( field === "popularity"){
+    return data
+  }
+  return  data.slice().sort(function (a, b) {
+    if (a[field] > b[field]) {
+      return 1;
+    }
+    if (a[field] < b[field]) {
+      return -1;
+    }
+    // a must be equal to b
+    return 0;
+  });
+
+};
+
+export const filterData = (data, filters) =>{
+  const especies = filters["species"];
+  if( especies.length){
+    data = data.filter((item) => especies.includes(item.species.toLowerCase()));
+  } 
+  return data
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
-import { especieF} from './data.js';
+import { especieF, orderData, filterData } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
-const dataTarjetas = document.querySelector(".cardWrapper");
-const spc = especieF(data.results);
-dataTarjetas.innerHTML = "";
-let todos = "";
-let especie1 = "";
+const filters = {
+  species: [],
+};
+const sorting = {
+  field: "popularity",
+}; 
 
-
-function pintarCards(personajes){
-  let cards="";
-  for ( let personaje of personajes) {
-  cards+= `<div class="card">
+function pintarCards(personajes, campos = ["name", "species", "status", "type", "gender"]) {
+  let cards = "";
+  personajes = filterData(personajes, filters)
+  personajes = orderData(personajes, sorting) 
+  for (let personaje of personajes) {
+    let contenidoCards = "";
+    for (let campo of campos) {
+      contenidoCards += `<a>${personaje[campo]}</a>
+      `    }
+    cards += `<div class="card">
   <div class="imageContent">
     <span class="overlay"></span>
     <div class="cardImage">
@@ -20,72 +26,52 @@ function pintarCards(personajes){
     </div>
     </div>
     <div class="cardContent">
-    <p class="name">${personaje.name}</p>
-    <a class="species">${personaje.species}</a>
-    <a class="status">${personaje.status}</a> 
-    <a class="status">${personaje.origin.name}</a> 
+    ${contenidoCards}
     </div>
     </div>`
-  } 
+    
+  }
+  const cardsCharacters= document.getElementById("characters");
+  cardsCharacters.innerHTML = cards;
   return cards
-    } 
-  
-  todos = pintarCards(data.results);
-  const divTodos = document.createElement("div");
-  divTodos.innerHTML = todos;
-  document.getElementById("principal").appendChild(divTodos);
+}
 
-  const fSpecie = document.getElementById('esp');
-  fSpecie.addEventListener("click", function(){
-    document.getElementById("especie").style.display = "flex"
-    document.getElementById("principal").style.display ="none"
-    document.getElementById("ordenar").style.display = "none"
-    document.getElementById("especie").innerHTML =""; //limpiar lo que haya antes
-    //crear boton
-    const botonSoloHumanos = document.createElement("button")
-    botonSoloHumanos.innerText = "Filtrar solo humanos";
-    document.getElementById("especie").appendChild(botonSoloHumanos);
-    //crear tarjetas
-    const crearTarjetas = document.createElement("div");
-    document.getElementById("especie").appendChild(crearTarjetas);
-    //filtrar por boton
-    botonSoloHumanos.addEventListener("click", () =>{
-      console.log("click");
-      //crearTarjetas.innerHTML = especie1; //.todos -> nueva variable
-      console.log(spc); //crearTarjetas.innerHTML = "variabe"
-      for ( let filter1 of spc) {
-        //pintar el html con las cards con los filtros de la funciòn en data
-            especie1 +=` <div class="card">
-            <div class="imageContent">
-              <span class="overlay"></span>
-              <div class="cardImage">
-                <img src="${filter1.image}" alt="" class="cardImg">
-              </div>
-            </div>
-            <div class="cardContent">
-            <p class="species">${filter1.name}</p>
-            <a class="species">${filter1.species}</a>
-            <a class="status">${filter1.gender}</a> 
-            </div>
-            </div>`
-          } crearTarjetas.innerHTML = especie1;
-          document.getElementById("especie").appendChild(crearTarjetas);
-    })
+pintarCards(data.results);
+
+const fSpecie = document.getElementById('esp');
+fSpecie.addEventListener("click", function () {
+  //crear boton
+  const botonSoloHumanos = document.createElement("button")
+  botonSoloHumanos.innerText = "Filtrar solo humanos";
+  document.getElementById("especie").appendChild(botonSoloHumanos);
+  //filtrar por boton
+  botonSoloHumanos.addEventListener("click", () => {
+    console.log("click");
+    //crearTarjetas.innerHTML = especie1; //.todos -> nueva variable
+    const spc = especieF(data.results);
+    console.log(spc); //crearTarjetas.innerHTML = "variabe"
+    pintarCards(spc);
   })
-  
-  //const crearTarjetas.innerHTML = "";
-  const crearTarjetas = document.createElement("div");
-  
-  const fall = document.getElementById('allp');
-  fall.addEventListener("click", function(){
-  document.getElementById("principal").style.display = "flex"
-  document.getElementById("ordenar").style.display = "none"
-  document.getElementById("especie").style.display = "none"
+})
 
-  const ord = document.getElementById('ordenr');
-ord.addEventListener("click", function(){
-  document.getElementById("ordenar").style.display = "flex"
-  document.getElementById("principal").style.display ="none"
-  document.getElementById("especie").style.display = "none"
-})
-})
+const ordenar = document.getElementById("sorting");
+
+ordenar.addEventListener("change", function() {
+  sorting.field = ordenar.value
+  pintarCards(data.results)
+});
+
+
+// Select all checkboxes with the name 'settings' using querySelectorAll.
+const checkboxes = document.querySelectorAll("input[type=checkbox][name=species]");
+
+// Use Array.forEach to add an event listener to each checkbox.
+checkboxes.forEach(function(checkbox) {
+  checkbox.addEventListener('change', function() {
+    filters.species=
+      Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
+      .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+      .map(i => i.value) // Use Array.map to extract only the checkbox values from the array of objects.
+      pintarCards(data.results);
+  }) 
+});
