7c9ef98e836d493c219dc5bcb44f28d175524939
valentinagutierrez <valentinagutierrez882@gmail.com>
2023-07-25 20:39:35 -0500

    primera prueba con jest

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,14 +1,9 @@
 import { especieF } from '../src/data.js';
 
 
-describe('data.especieF', () => {
+describe('es una funcion', () => {
   it('is a function', () => {
     expect(typeof especieF).toBe('function');
   });
-
-  it('returns `especieF`', () => {
-    expect(especieF()).toBe('especieF');
-  });
 });
-
 
