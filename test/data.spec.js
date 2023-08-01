106e174c72f33fbe120da0f6e16c1023e74e73cd
valentinagutierrez <valentinagutierrez882@gmail.com>
2023-07-31 23:29:44 -0500

    dos pruebas con los test

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,9 +1,48 @@
-import { especieF } from '../src/data.js';
+import { especieF, orderData } from '../src/data.js';
+// const con array c
+const data = [{
+  "name": "Rick Sanchez",
+  "status": "Alive",
+  "species": "Human",
+  "type": "",
+  "gender": "Male",
+}, {
+  "name": "Beth Smith",
+  "status": "Alive",
+  "species": "Human",
+  "type": "",
+  "gender": "Female",
+}, {
+  "name": "Risotto Groupon",
+  "status": "Dead",
+  "species": "Alien",
+  "type": "Blue ape alien",
+  "gender": "Male",
+}, {
+  "name": "Scary Brandon",
+  "status": "Alive",
+  "species": "Humanoid",
+  "type": "Monster",
+  "gender": "Male",
+}
+]
 
+const sorting = {
+  field: "popularity",
+};
 
 describe('es una funcion', () => {
   it('is a function', () => {
     expect(typeof especieF).toBe('function');
   });
+  it("la lista contiene la especie de los personajes", () => {
+    expect(especieF(data)[0].name).toEqual("Rick Sanchez");
+    expect(especieF(data)[1].name).toEqual("Beth Smith");
+  });
 });
 
+describe('El valor de field es pupularity', () => {
+  it('debería retornar popularity', () => {
+    expect(orderData(sorting)[0].field).toEqual("popularity");
+  });
+});
